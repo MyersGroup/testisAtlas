e7a1e6023fe71833f30d1d3c1e11fc17b51e8b71
daniel-wells <daniel-wells@users.noreply.github.com>
2019-05-27 19:34:43 +0100

    Update setup_low_memory_mode.R
    simplify by specifying inclusion rather than exclusion

--- shinyApp/setup_low_memory_mode.R
+++ shinyApp/setup_low_memory_mode.R
@@ -5,44 +5,15 @@ data <- as.matrix(data)
 data <- as.big.matrix(data, backingfile = "data.big")
 data <- attach.big.matrix("data.big.desc")
 
-# keep these as easter eggs
-#cell_data$group <- NULL
-#cell_data$msci_ratio <- NULL
-#cell_data$PseudoTime <- NULL
 cell_data$log_library_size <- log10(cell_data$library_size)
-
-cell_data$Tsne1 <- NULL
-cell_data$Tsne1_QC2 <- NULL
-cell_data$Tsne2 <- NULL
-cell_data$Tsne2_QC2 <- NULL
-cell_data$experiment <- NULL
-cell_data$library_size <- NULL
-cell_data$somatic <- NULL
-cell_data$somatic2 <- NULL
-cell_data$somatic3 <- NULL
-# keep somatic4 as somatic
 cell_data$somatic <- cell_data$somatic4
-cell_data$somatic4 <- NULL
-cell_data$sex <- NULL
-cell_data$sex_predictions <- NULL
-cell_data$autosomal <- NULL
-cell_data$autosomal_predictions <- NULL
-cell_data$X_predictions <- NULL
-cell_data$Y_predictions <- NULL
-cell_data$PseudoTime1 <- NULL
-cell_data$PseudoTime2 <- NULL
-cell_data$hclust_group <- NULL
-cell_data$`mt-Rnr2` <- NULL
-cell_data$`mt-Rnr2_pred` <- NULL
-
-str(cell_data)
+cell_data <- cell_data[,c("cell","Tsne1_QC1","Tsne2_QC1","PseudoTime","msci_ratio","group","Umap1","Umap2","log_library_size","somatic",paste0("V",1:50)), with=F]
 
 principal_curves <- principal_curves["df_9"]
-
 str(principal_curves[["df_9"]])
 
 library(data.table)
 library(testisAtlas)
 load_component_orderings()
 
-save(chromosome.lengths, cell_data, principal_curves, SDAresults, rna_locations, component_order_dt, file = "cached_objects_small.rds")
+save(cell_data, principal_curves, SDAresults, gene_annotations, component_order_dt, file = "cached_objects_small.rds")
